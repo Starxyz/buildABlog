6305ad92160a3b3dc62aced8005e7f6ca27ad356
Starxy <a136009394@163.com>
2018-11-18 18:56:54 +0800

    test thymeleaf, completed

---
+++ xmlTest/src/main/java/com/waylau/spring/boot/blog/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package com.waylau.spring.boot.blog.repository;
+
+import com.waylau.spring.boot.blog.domain.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    /**
+     * 新增或者修改用户
+     * @param user
+     * @return
+     */
+    User saveOrUpdateUser(User user);
+
+    /**
+     * 删除用户
+     * @param id
+     */
+    void deleteUser(Long id);
+
+    /**
+     * 根据用户 id 获取用户
+     * @param id
+     * @return
+     */
+    User getUserById(Long id);
+
+    /**
+     * 获取所有用户的列表
+     * @return
+     */
+    List<User> listUser();
+}
