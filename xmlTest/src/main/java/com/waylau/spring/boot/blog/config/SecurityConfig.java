e798910c71ea962f5563748948f388eaf5beaed1
Starxy <a136009394@163.com>
2018-11-22 23:19:24 +0800

    test spring security with thymeleaf, some bugs, version not match

---
+++ xmlTest/src/main/java/com/waylau/spring/boot/blog/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.waylau.spring.boot.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    /**
+     * 自定义配置
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/fonts/**", "/index")
+                .permitAll()
+                .antMatchers("/users/**")
+                .hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login-error");
+    }
+
+    /**
+     * 认证信息管理
+     * @param auth
+     * @throws Exception
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("waylau")
+                .password("123456")
+                .roles("ADMIN");
+    }
+}
